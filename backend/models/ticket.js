7f8b56a90425c7be04e240430cdcfd17f5f924c8
george-condeescu <george_condeescu@yahoo.com>
2026-03-26 18:40:24 +0200

    problema cu: Ticket.user_id cannot be null"

--- backend/models/ticket.js
+++ backend/models/ticket.js
@@ -27,7 +27,7 @@ const Ticket = sequelize.define(
     },
     user_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
     },
     fisiere: {
       type: DataTypes.JSON,
