20d6bd5060c4da2f77d8db93006aa3781841cc3b
george-condeescu <george_condeescu@yahoo.com>
2026-05-11 10:01:23 +0300

    schimbat baseURL

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api',
+  //baseURL: import.meta.env.VITE_API_URL || '/api',
+  baseURL: import.meta.env.VITE_API_URL || '/tailwind/api',
   timeout: 10000, // 10 secunde pana la timeout
   headers: {
     'Content-Type': 'application/json',
